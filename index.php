a40f2ca6867d3c953149e913f33b28e953808b1f
alessandro-cordoglio <alessandro.cord20@gmail.com>
2022-12-15 17:16:26 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,39 @@
+<!-- Milestone 1
+Creare un form che invii in GET la lunghezza della password. Una nostra funzione utilizzerà questo dato per generare una password casuale (composta da lettere, lettere maiuscole, numeri e simboli) da restituire all’utente.
+Scriviamo tutto (logica e layout) in un unico file index.php
+
+Milestone 2
+Verificato il corretto funzionamento del nostro codice, spostiamo la logica in un file functions.php che includeremo poi nella pagina principale
+
+Milestone 3
+Invece di visualizzare la password nella index, effettuare un redirect ad una pagina dedicata che tramite $_SESSION recupererà la password da mostrare all’utente.
+
+Milestone 4 (BONUS)
+Gestire ulteriori parametri per la password: quali caratteri usare fra numeri, lettere e simboli. Possono essere scelti singolarmente (es. solo numeri) oppure possono essere combinati fra loro (es. numeri e simboli, oppure tutti e tre insieme).
+Dare all’utente anche la possibilità di permettere o meno la ripetizione di caratteri uguali. -->
+<?php
+    $letters[]='abcdefghijklmnopqrstuvxwyz';
+    $numbers[]='1234567890';
+    $numbers[]='\!"£$%&/()=?^@#';
+
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+    <title>Password generator</title>
+</head>
+<body>
+    <h1>GENERA UNA PASSWORD SICURA</h1>
+    <form class="form p-2 mt-4" action="index.php" method="GET">
+        <label for="pswlg">Scegli lunghezza password:</label>
+        <input name="pswlg" id="password" type="text">
+        <button class="btn btn-primary" type="submit">Genera</button>
+    </form>
+</body>
+</html>
